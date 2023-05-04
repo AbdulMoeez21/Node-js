fe7500c217f5f79c562e41107901da2e2b0bb934
AbdulMoeez21 <moizrao7@gmail.com>
2023-05-04 23:12:14 +0500

    initial

--- index.js
+++ index.js
@@ -73,20 +73,21 @@
 
 
 import express from "express";
- import path from "path";
+// import path from "path";
 
 const app = express();
- const pathh = path;
+import path from 'path';
+const pathh = path;
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+
 
 app.get('/', (req, res) => {
-    // res.send('<h2> Hello from Express js</h2>')
-    res.sendFile(pathh.resolve(__dirname)+'/index.html')
+    // res.send('<h2> Hello Express js</h2>')
+ res.sendFile(pathh.join(__dirname,"public","index.html"))
 });
-
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
